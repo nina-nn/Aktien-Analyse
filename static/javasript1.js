41fa7236f50452f48ab871eb1ae65f7d650c3ef1
Nina <nina.neermann@icloud.com>
2021-11-28 13:17:06 +0100

    Basic HTML-Layout/CSS and NavBar for two viewports Create header for small viewport with dropdown menu and header for larger viewport with navigation in header and no dropdown menu

---
+++ static/javasript1.js
@@ -0,0 +1,19 @@
+/*=======================================================*/
+/*JS FOR CSS*/
+/*=======================================================*/
+
+/*Hängt dem übergebenen Element(p) die .change class an wenn noch nicht vorhanden, sonst entfernt die Funktion toggle diese*/
+function menuMobileIconChange(p) {
+    p.classList.toggle("change");
+  }
+
+/*=======================================================*/
+/*JS FOR API*/
+/*=======================================================*/
+
+
+
+
+/*=======================================================*/
+/*JS FOR STOCKS ANALASIS*/
+/*=======================================================*/
